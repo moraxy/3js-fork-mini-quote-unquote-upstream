4af3e33d5366af509028bbceea3d6b511fe1b794
3botjs <3botjs>
2018-02-17 14:04:28 +0100

    Mini'ed fork of three.js for testing purposes

---
+++ examples/files.js
@@ -0,0 +1,38 @@
+var files = {
+	"webgl": [
+		"webgl_geometry_colors",
+		"webgl_geometry_cube",
+		"webgl_loader_fbx"
+	],
+	"webgl / advanced": [
+		"webgl_worker_offscreencanvas"
+	],
+	"webgl deferred": [
+		"webgldeferred_animation"
+	],
+	"webaudio": [
+		"webaudio_visualizer"
+	],
+	"webvr": [
+		"webvr_cubes"
+	],
+	"misc": [
+		"misc_lookat",
+        "misc_uv_tests"
+	],
+	"css3d": [
+		"css3d_molecules"
+	],
+	"canvas": [
+		"canvas_ascii_effect"
+	],
+	"raytracing": [
+		"raytracing_sandbox"
+	],
+	"software": [
+		"software_geometry_earth"
+	],
+	"svg": [
+		"svg_lines"
+	]
+};
